aadd90d7e0043dba897d79c3cdac1189d4c5faec
Chayce Knaub <chknaub@yahoo.com>
2020-07-12 19:08:14 -0400

    fresh start

---
+++ README.md
@@ -0,0 +1,13 @@
+# Blogful API!
+
+## Scripts
+
+Start the application `npm start`
+
+Start nodemon for the application `npm run dev`
+
+Run the tests in watch mode `npm test`
+
+## Deploying
+
+When your new project is ready for deployment, add a new heroku application with `heroku create`. This will make a new git remote called "heroku" and you can then `npm run deploy` which will push to this remote's master branch.
---
+++ src/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const articlesRouter = require('./articles/articles-router')
+
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
+
+app.use('/api/articles', articlesRouter)
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
+
+module.exports = app
